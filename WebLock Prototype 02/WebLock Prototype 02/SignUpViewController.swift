cbecb61587fe737042272cf3d947ec5cc0f68ed3
shemahaatwit <55283153+shemahaatwit@users.noreply.github.com>
2023-03-10 12:11:42 -0800

    Basic Login

---
+++ WebLock Prototype 02/WebLock Prototype 02/SignUpViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SignUpViewController.swift
+//  WebLock Prototype 02
+//
+//  Created by allanshemah on 3/9/23.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func createaccountTapped(_ sender: Any) {
+        if email.text?.isEmpty == true{
+            print("no text in email field")
+            return
+        }
+        if password.text?.isEmpty == true {
+            print("no text in password field")
+            return
+        }
+        signup()
+    }
+    func signup(){
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (authResult, error) in
+            guard let user = authResult?.user, error == nil else{
+                print("error")
+                return
+            }
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "Home Page")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+        
+    }
+    
+}
---
+++ WebLock Prototype 02/WebLock Prototype 02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebLock Prototype 02
+//
+//  Created by allanshemah on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
