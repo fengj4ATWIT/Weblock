98ab4791908d8571854ad1dd3542a406d5fecdf8
xud1atwit <92702858+xud1atwit@users.noreply.github.com>
2023-03-14 07:15:44 -0400

    Update Servers.swift

--- WebLock Prototype 02/WebLock Prototype 02/Servers.swift
+++ WebLock Prototype 02/WebLock Prototype 02/Servers.swift
@@ -16,10 +16,5 @@ struct Server: Identifiable {
 
 var servers = [
     
-    Server(name: "United States", flag: "us"),
-    Server(name: "India", flag: "in"),
-    Server(name: "United Kingdom", flag: "uk"),
-    Server(name: "France", flag: "fr"),
-    Server(name: "Germany", flag: "ge"),
-    Server(name: "Singapore", flag: "si"),
+    Server(name: "Raspberry PI", flag: "raspberry-pi-logo"),
 ]
